48883559b5bd55e1084e4c214d38b877c7fed92f
samer <samersoltani@gmail.com>
2025-07-08 15:18:33 +0330

    Set a dynamic ALLOWED_HOSTS for Render

--- stor/stor/settings.py
+++ stor/stor/settings.py
@@ -27,7 +27,15 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['my-django-store.onrender.com', 'localhost']
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+# برای اطمینان، دامنه اصلی را هم به صورت دستی اضافه می‌کنیم
+ALLOWED_HOSTS.append('my-django-store.onrender.com')
+
 
 
 # Application definition
