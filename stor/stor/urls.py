12d69f2d623aa223041d4513285d9929f8bb13b6
samer <samersoltani@gmail.com>
2025-07-09 02:37:57 +0330

    Finalize and clean up production urls.py

--- stor/stor/urls.py
+++ stor/stor/urls.py
@@ -2,20 +2,16 @@
 URL configuration for stor project.
 """
 from django.contrib import admin
-from django.urls import path, include ,re_path
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
 from django.views.static import serve
-
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
     path('account/', include('account.urls')),
     path('cart/', include('cart.urls', namespace='cart')),
+    
+    # این تنها کد مورد نیاز برای نمایش فایل‌های Media در محیط آنلاین است
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-# این خط برای نمایش تصاویر آپلودی (media) در محیط آنلاین ضروری است
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
