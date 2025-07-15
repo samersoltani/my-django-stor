293d27f2d56426089238882f2b50f790816c3cc9
samer <samersoltani@gmail.com>
2025-07-15 15:04:07 +0330

    Simplify urls.py for Cloudinary

--- stor/stor/urls.py
+++ stor/stor/urls.py
@@ -1,17 +1,9 @@
-"""
-URL configuration for stor project.
-"""
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.conf import settings
-from django.views.static import serve
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
     path('account/', include('account.urls')),
     path('cart/', include('cart.urls', namespace='cart')),
-    
-    # این تنها کد مورد نیاز برای نمایش فایل‌های Media در محیط آنلاین است
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
