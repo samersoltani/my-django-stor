af2c7578090451c3818f038813565f1c9971bb86
samer <samersoltani@gmail.com>
2025-07-15 13:15:06 +0330

    Finalize settings.py for production

--- stor/stor/settings.py
+++ stor/stor/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # در محیط آنلاین، دیباگ همیشه باید خاموش باشد
-DEBUG = True 
+DEBUG = False 
 
 # تنظیمات پیشرفته و امن برای هاست‌های مجاز
 ALLOWED_HOSTS = []
